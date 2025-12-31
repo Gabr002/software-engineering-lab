ef7ec511c6b9de3bce685153ae91bcc3a2164ac5
dantas <gdantaslopes62@gmail.com>
2025-12-31 07:08:24 -0300

    feat: up all files

--- typescript/src/functions.ts
+++ typescript/src/functions.ts
@@ -1,5 +1,9 @@
-const sum = (x: number, y: number): string | number => {
-    return (x + y).toString();
+interface MathFunc {
+    (x: number, y: number): number;
+}
+
+const sum: MathFunc = (x: number, y: number): number => {
+    return x + y;
 }
 
 const value = sum(2, 3);
@@ -9,3 +13,22 @@ const log = (message: string): void | number => {
 
     return 1;
 }
+
+// Como usar interfaces junto com funções
+
+
+// Ou seja, toda função que seguir a estrutura de MathFunc deve receber dois parâmetros do tipo number e retornar um number
+
+const sub : MathFunc = (x: number, y: number): number => {
+    return x - y;
+}
+
+const multiply : MathFunc = (x: number, y: number): number => {
+    return x * y;
+}   
+
+const divide : MathFunc = (x: number, y: number): number => {
+    return x / y;
+}
+
+// No código acima, definimos uma interface MathFunc que descreve a estrutura de uma função que recebe dois parâmetros do tipo number e retorna um number. Em seguida, criamos várias funções (sum, sub, multiply, divide) que implementam essa interface. Isso garante que todas essas funções sigam a mesma assinatura, promovendo consistência e facilitando a manutenção do código.
--- typescript/src/objects.ts
+++ typescript/src/objects.ts
@@ -10,29 +10,33 @@ type User = {
     email?: string; // Propriedade opcional
     password?: string;
     orders: Order[];
+    register?(): string;
 };
 
-const user : User = {
+const user: User = {
     firstName: "Gabriel",
     age: 25,
-    orders: [{productId: '1', price: 200}]
+    orders: [{ productId: '1', price: 200 }]
+    // register() {
+    //     return "Usuário registrado com sucesso!";
+    // },
 }
 
 
 // Estamos definindo objetos com tipos específicos usando a palavra-chave 'type' em TypeScript. No exemplo acima, criamos um tipo 'User' que possui propriedades obrigatórias 'firstName' e 'age', e propriedades opcionais 'email' e 'password'. Em seguida, criamos um objeto 'user' do tipo 'User', fornecendo apenas as propriedades obrigatórias. Isso demonstra como podemos usar tipos personalizados para garantir que nossos objetos sigam uma estrutura específica, enquanto ainda permitimos flexibilidade com propriedades opcionais.
 
-const printLog = (message: string) => {}
+const printLog = (message: string) => { }
 
 printLog(user.password!)
 
 // A exclamação (!) após 'user.password' é o operador de asserção de não nulo em TypeScript. Ele informa ao compilador que temos certeza de que 'user.password' não é nulo ou indefinido naquele ponto do código. Isso é útil quando sabemos que uma propriedade opcional foi definida, mas o compilador não consegue inferir isso automaticamente. No exemplo acima, estamos passando 'user.password' para a função 'printLog', e a exclamação garante que não haverá erros de compilação relacionados a valores nulos ou indefinidos. Já quando usamos 'message?: string' na definição da função, estamos indicando que o parâmetro 'message' é opcional e pode ser omitido ao chamar a função.
 
 // Unions
-type Author  = {
+type Author = {
     books: string[];
 }
 
-const author: Author & User = { 
+const author: Author & User = {
     age: 40,
     books: ['1'],
     email: "author@com.br",
@@ -70,4 +74,5 @@ type Grade = number | string;
 const grade: Grade = 10;
 
 
-// 51:36 - segundos da aula
+// 55:33 - segundos da aula
+
