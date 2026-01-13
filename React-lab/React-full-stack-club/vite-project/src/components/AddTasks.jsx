de2499c9c53587db52c5044d014b8a40f654874b
dantas <gdantaslopes62@gmail.com>
2026-01-12 22:12:45 -0300

    up all files

---
+++ React-lab/React-full-stack-club/vite-project/src/components/AddTasks.jsx
@@ -0,0 +1,8 @@
+function AddTasks(props) { // props é um objeto que contém todas as propriedades passadas para o componente AddTasks. Ele permite que você passe dados de um componente pai para um componente filho.
+    // console.log(props); // Isso imprime no console do navegador o objeto props, permitindo que você veja quais propriedades foram passadas para o componente.   
+    return (
+      <h1>Adicionar Tarefas</h1>  
+    );
+}
+  
+export default AddTasks;
--- React-lab/React-full-stack-club/vite-project/src/main.jsx
+++ React-lab/React-full-stack-club/vite-project/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// Módulos e importações adicionais podem ser adicionados aqui
+
+createRoot(document.getElementById('root')).render( // O que esse código faz? Ele cria a raiz da aplicação React e a renderiza no elemento HTML com o ID 'root'.
+  
+  
+  // Quando falar que o componente App é renderizado, significa que ele está sendo exibido na tela dentro do elemento root.
   <StrictMode>
-    <App />
+    <App /> 
   </StrictMode>,
 )
